5f6f631e47fbad36ac804f0763d05c23022aa147
Priyavrat Kumar <122096583+Priyavrat001@users.noreply.github.com>
2024-09-20 19:09:24 +0530

    Created Query in transaction.resolver

--- resolver/transaction.resolver.js
+++ resolver/transaction.resolver.js
@@ -1,10 +1,40 @@
-import {transactions} from "../dummyData/data.js"
+import { Transaction } from "../models/transaction.model.js";
 
 const transactionResolver = {
     Query: {
-        transactions: ()=>{
-            return transactions
+        transactions: async (_, args, context) => {
+            try {
+
+                if (!context.getUser) throw new Error("Unautherized");
+
+                const userId = await context.getUser()._id;
+
+                const transactions = await Transaction.find({ userId });
+
+                return transactions;
+
+            } catch (err) {
+                console.error("Something went wrong in transactions", err);
+
+                throw new Error(err.message || "Internal server error");
+
+            }
+        },
+        transaction:async(_, {transactionId})=>{
+            try {
+                const transaction = await Transaction.findById(transactionId);
+
+                return transaction;
+            } catch (err) {
+
+                console.error("Something went wrong in transaction", err);
+
+                throw new Error(err.message || "Internal server error");
+                
+            }
+
         }
+        // todo add category statics
     },
     Mutation: {}
 }
